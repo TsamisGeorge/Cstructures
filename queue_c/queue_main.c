7fb4d41927522ae04fd917ac24618793a95d788c
George_Tsamis <george.tsamis.cs@gmail.com>
2023-03-31 22:27:33 +0300

    finished the static queue

--- queue_c/queue_main.c
+++ queue_c/queue_main.c
@@ -5,10 +5,11 @@ int main(void)
 {
 	QUEUE qu;
 	QU_init(&qu);
-	int x;
+	elem x;
+	int choice, i;
 	while(1)
 	{
-		printf("|Queue Menu|.\n");
+		printf("\n|Queue Menu|\n");
 		printf("____________\n");
 		printf("1. Enqueue\n");
 		printf("2. Dequeue\n");
@@ -19,8 +20,31 @@ int main(void)
 		switch(choice)
 		{
 			case 1:
-				printf("Give an element: ");
+				printf("\nGive an element: ");
+				scanf("%d",&x);
 				if(QU_enqueue(&qu, x))
+					printf("\nEnqueued the element: %d.", x);
+				else
+					printf("\nFailed the enqueue, stack is full.");
+				break;
+			case 2:
+				if(QU_dequeue(&qu, &x))
+					printf("\nDequeued the element: %d", x);
+				else
+					printf("\nFailed to dequeue stack is empty.");
+				break;
+			case 3:
+				printf("\n");
+				for(i=0;i<=qu.finish;i++)
+				{
+					printf("[%-2d]",qu.array[i]);	
+				}
+				break;
+			case 4:
+				printf("\nExited.");
+				exit(0);
+			default:
+				printf("\nInvalid input.");
 		}
 	}
 }
